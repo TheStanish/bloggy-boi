074cff7eaaf7c072c23c5eacf78d35726630be21
TheStanish <tstan2310@gmail.com>
2022-05-18 17:42:12 -0400

    added styling using bootstrap, jquery, popper. Started getting super srs.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_16_180125) do
+ActiveRecord::Schema.define(version: 2022_05_18_193510) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
